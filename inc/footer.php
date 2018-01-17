6d5ab5f6ae7eaeda3b7f3e11e10e246520882b10
kamrul <kamrulhassan1361@gmail.com>
2018-01-17 16:49:17 +0600

    basic design for file upload done

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+</section>
+		<section class="footeroption">
+			<h2>www.techkamrul.com</h2>
+		</section>
+	</div>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Uploading Image Files</title>
+	<style>
+		body{
+			font-family: verdana;
+		}
+
+		.phpcoding{
+			width: 900px;
+			margin: 0 auto;
+			background-color: #ddd;
+
+		}
+
+		.headeroption, .footeroption{
+			background: #444;
+			color: #fff;
+			text-align: center;
+			padding: 20px;
+		}
+
+		.headeroption h2, .footeroption h2{
+			margin: 0;
+		}
+
+		.mainoption{
+			min-height: 420px;
+			padding: 20px;
+		}
+
+		.myform{
+			width: 500px;
+			border: 1px solid #999;
+			margin: 0 auto;
+			padding: 10px;
+		}
+
+		input[type="submit"], input[type="file"]{
+			cursor: pointer;
+		}
+	</style>
+</head>
+<body>
+	<div class="phpcoding">
+		<section class="headeroption">
+			<h2>Uploading Image File with PHP</h2>
+		</section>
+		<section class="mainoption">
